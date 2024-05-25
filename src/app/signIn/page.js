3a914f934c05053b1c7484b0f2bdb24944e705dc
daniellee6402 <daniellee6402@gmail.com>
2024-05-24 17:24:51 -0700

    sign in form

---
+++ src/app/signIn/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignInForm from '../components/SignInForm'
+
+const SignInPage = () => {
+  return (
+    <div>
+        <h1>Signjjjjj In Page</h1>
+        <SignInForm/>
+    </div>
+  )
+}
+
+export default SignInPage
